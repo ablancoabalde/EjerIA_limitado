e33d6be5b34f70c66503a2803933b304d7e8dd58
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-11-09 09:04:31 +0100

    Creo class muestra

--- EjerIA/src/ejeria/Muestra.java
+++ EjerIA/src/ejeria/Muestra.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejeria;
 
-/**
- *
- * @author ablancoabalde
- */
 public class Muestra {
+    
+    public Muestra(){
+        
+    }
+    
+    public void mostratNombre(String nom){
+        System.out.println("Hola " + nom + " soy una IA limitada");
+        
+    }
     
 }
